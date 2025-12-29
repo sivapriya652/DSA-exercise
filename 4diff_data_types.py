690c8b82d4af612c2e0ba3dde1904ba62d44453c
sivapriya652 <sivapriya6527@gmail.com>
2025-12-29 21:36:12 +0530

    Create 4diff_data_types.py

---
+++ 4diff_data_types.py
@@ -0,0 +1,9 @@
+#Create 4 variables of different data types
+age=18   # integer
+height=163.5  # float
+name="Sivapriya.S"  #string
+are_you_student= True  # boolean
+print("my name is:",name)
+print("my age is:",age)
+print("my height is :",height)
+print("i am a student:",are_you_student)
