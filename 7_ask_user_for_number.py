d0f5d54ba9dc1f9b426ec9d3bfff4dc63edadf65
sivapriya652 <sivapriya6527@gmail.com>
2025-12-29 21:44:06 +0530

    Create 7_ask_user_for_number.py

---
+++ 7_ask_user_for_number.py
@@ -0,0 +1,8 @@
+#Ask user for a number
+number=int(input("enter a number:"))
+if number>0:
+    print("the number is positive")
+elif number<0:
+    print("the number is negative")
+else:
+    print("the number is zero")
