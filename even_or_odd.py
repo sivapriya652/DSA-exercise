33f684610f506366758185bfc5b204a118611eca
sivapriya652 <sivapriya6527@gmail.com>
2025-12-29 21:40:01 +0530

    Create even_or_odd.py

---
+++ even_or_odd.py
@@ -0,0 +1,6 @@
+#to find whether given number is even or not
+number=int(input("enter your favourite number:"))
+if number%2==0:
+    print("the number is even!!!")
+else:
+    print("the number is odd!!!")
