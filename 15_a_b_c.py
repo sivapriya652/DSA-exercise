2f517344e741124dd23c6aaec55f85086e273854
sivapriya652 <sivapriya6527@gmail.com>
2025-12-29 23:11:51 +0530

    Create 15_a_b_c.py

---
+++ 15_a_b_c.py
@@ -0,0 +1,14 @@
+number=[12, 45, 3, 98, 7, 34, 21]
+count=0
+for i in number:
+    # a
+    print(i)
+print("this above answers for A")    
+for i in number:
+    #b
+    if i>30:
+        count=count+1
+        print(i)
+print("this above answers for B")
+print(count)
+print("this above answers for C")
