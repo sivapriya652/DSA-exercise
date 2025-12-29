989ba528fe04920c9bd6e45655d86ef4e525123b
sivapriya652 <sivapriya6527@gmail.com>
2025-12-29 21:39:01 +0530

    Create final_price.py

---
+++ final_price.py
@@ -0,0 +1,7 @@
+#Calculate the final price after discount
+price=599.99
+discount=0.15
+discount_amount=price*discount
+amount=price-discount_amount
+final_price=round(amount,2)
+print("the final price is:",final_price)
